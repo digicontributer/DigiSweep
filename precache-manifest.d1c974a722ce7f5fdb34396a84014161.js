e6aaa7538485e1ae6f6146bc641f7a35e70515bc
digicontributer <esot@digibyte.io>
2019-08-19 07:31:27 +1000

    Updates

---
+++ precache-manifest.d1c974a722ce7f5fdb34396a84014161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43e5c820b1a90334cb6ad674b7863c8",
+    "url": "/digisweep/index.html"
+  },
+  {
+    "revision": "ce2b8627bee25093fdd0",
+    "url": "/digisweep/static/css/main.50a7ae4d.chunk.css"
+  },
+  {
+    "revision": "35c8cdb8a7c0205918ce",
+    "url": "/digisweep/static/js/2.e2524190.chunk.js"
+  },
+  {
+    "revision": "ce2b8627bee25093fdd0",
+    "url": "/digisweep/static/js/main.3e12dd60.chunk.js"
+  },
+  {
+    "revision": "4840e23f7102f41510e0",
+    "url": "/digisweep/static/js/runtime~main.add04cae.js"
+  }
+]);
